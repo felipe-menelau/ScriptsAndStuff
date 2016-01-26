2c17f056cb87e507cabf7676e2ca37b443e9236d
FelipeMenelau <felipe.menelau@inlocomedia.com>
2016-01-26 17:49:59 -0300

    Removed Text Files

--- CSVfun/removedoubles.py
+++
@@ -1,9 +0,0 @@
-
-def removedoubles(inf, outf):
-	lines_seen = set()
-	outfile = open(outf, "w+")
-	for line in open(inf, "r"):
-		if line not in lines_seen:
-			outfile.write(line)
-			lines_seen.add(line)
-	outfile.close()
